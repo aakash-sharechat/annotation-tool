2aa02b3ddeace957c5e438008923dd6d99381797
Aakash Amish <theawesomeaakash@gmail.com>
2020-02-04 15:52:48 +0530

    Updates

---
+++ precache-manifest.e347a555643b06eb50b724b0d41d8e2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9883591a1d157b3954da43a36ba89e7e",
+    "url": "/annotation-tool/index.html"
+  },
+  {
+    "revision": "0ed6fd6fc3c1ab248b37",
+    "url": "/annotation-tool/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "e4c8b5bf2b329290d572",
+    "url": "/annotation-tool/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0ed6fd6fc3c1ab248b37",
+    "url": "/annotation-tool/static/js/2.2c2696b7.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/annotation-tool/static/js/2.2c2696b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c8b5bf2b329290d572",
+    "url": "/annotation-tool/static/js/main.7aa912bb.chunk.js"
+  },
+  {
+    "revision": "60e0bd377fdc2a58db47",
+    "url": "/annotation-tool/static/js/runtime-main.a796bf50.js"
+  }
+]);
